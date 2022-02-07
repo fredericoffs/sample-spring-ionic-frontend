4ba2ddcdc030067710f278f84b747b3b9ca07ccc
Frederico F Silva <fredericoffs@live.com>
2022-02-07 18:49:11 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { API_CONFIG } from '../../config/api.config';
-import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -12,10 +12,11 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 export class CategoriasPage {
 
   bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
   items: CategoriaDTO[];
 
   constructor(
-    public navCtrl: NavController,
+    public navCtrl: NavController, 
     public navParams: NavParams,
     public categoriaService: CategoriaService) {
   }
@@ -25,9 +26,6 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response;
       },
-        error => {
-          console.log(error);
-        });
+      error => {});
   }
-
 }
