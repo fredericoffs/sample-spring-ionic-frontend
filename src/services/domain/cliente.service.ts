a4c9a46f024c8eadbdc7f8191ee76b8ef605a52f
Frederico F Silva <fredericoffs@live.com>
2022-02-09 18:17:16 -0300

    Tratando erros 403

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
@@ -16,8 +16,8 @@ export class ClienteService {
         return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBucket(id : string) : Observable<any> {
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType : 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
