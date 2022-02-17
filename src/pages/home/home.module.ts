b68399d17602c397893227589e6bc4cfdf7ae4b3
Frederico F Silva <fredericoffs@live.com>
2022-02-16 22:54:11 -0300

    Tela de confirmação de pedido

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { HomePage } from './home';
 
 @NgModule({
-    declarations: [HomePage],
-    imports: [IonicPageModule.forChild(HomePage)]
+    declarations: [
+        HomePage
+    ],
+    imports: [
+        IonicPageModule.forChild(HomePage)
+    ]
 })
 export class HomeModule {
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,8 +11,11 @@ export class ClienteService {
 
     }
 
-    findByEmail(email: string) {
+    findById(id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
 
+    findByEmail(email: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
